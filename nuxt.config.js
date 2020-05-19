862cdc5be186ca6ec6fca2d5b588d15e33f52a7a
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-05-19 16:38:34 +0100

    routing

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,7 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
+    //  '@nuxtjs/eslint-module',
     '@nuxtjs/vuetify'
   ],
   /*
@@ -71,6 +71,6 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) { }
   }
 }
