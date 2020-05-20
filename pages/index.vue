9df11965328818cceabecb4bf3c6098f5819a581
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-05-20 23:34:55 +0100

    aysncData, promises use for ssr

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,128 @@
 <template>
-  <div>
-    <no-ssr>
-      <Home />
-    </no-ssr>
-  </div>
+  <no-ssr>
+    <v-container fluid>
+      <v-row>
+        <v-col cols="12" md="4" sm="4" v-for="post in postImages" :key="post.id">
+          <v-card>
+            <nuxt-link :to="'posts/' + post.id">
+              <v-img :src="post.imageURL" height="400">
+                <v-row align="end" wrap class="lightbox white--text pa-2 fill-height">
+                  <v-col>
+                    <div class="subheading float-left">
+                      {{ post.title }}
+                      <br />
+                      <span></span>
+                      <br />
+                      {{ post.date }}
+                    </div>
+                    <br />
+                    <div class="body-1">{{ post.content }}</div>
+                    <v-btn :to="'/posts/' + post.id" class="white--text" color="purple darken-4">
+                      Read More
+                      <v-icon>arrow_forward</v-icon>
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-img>
+            </nuxt-link>
+          </v-card>
+        </v-col>
+
+        <!-- <v-col cols="12" md="4" sm="4">
+          <v-card>
+            <nuxt-link :to="'posts/' +2">
+              <v-img src="https://cdn.vuetifyjs.com/images/carousel/planet.jpg" height="400">
+                <v-row align="end" wrap class="lightbox white--text pa-2 fill-height">
+                  <v-col>
+                    <div class="subheading float-left">
+                      post title
+                      <br />
+                      <span>2020-2-12</span>
+                      <br />
+                    </div>
+                    <br />
+                    <br />
+                    <div class="body-1">content</div>
+                    <v-btn :to="'/posts/' + 2" class="white--text" color="purple darken-4">
+                      Read More
+                      <v-icon>arrow_forward</v-icon>
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-img>
+            </nuxt-link>
+          </v-card>
+        </v-col>-->
+
+        <!-- <v-col cols="12" md="4" sm="4">
+          <v-card>
+            <nuxt-link :to="'/posts/' + 1">
+              <v-img src="https://cdn.vuetifyjs.com/images/carousel/bird.jpg" height="400">
+                <v-row align="end" wrap class="lightbox white--text pa-2 fill-height">
+                  <v-col>
+                    <div class="subheading float-left">
+                      post title
+                      <br />
+                      <span>2020-2-12</span>
+                      <br />
+                    </div>
+                    <br />
+                    <br />
+                    <div class="body-1">content</div>
+                    <v-btn :to="'/posts/' + 1" class="white--text" color="purple darken-4">
+                      Read More
+                      <v-icon>arrow_forward</v-icon>
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-img>
+            </nuxt-link>
+          </v-card>
+        </v-col>-->
+      </v-row>
+    </v-container>
+  </no-ssr>
 </template>
-
 <script>
-import Home from '~/components/Home.vue'
-
 export default {
-  components: {
-    Home
+  asyncData(context) {
+    return new Promise((resolve, rejected) => {
+      setTimeout(() => {
+        resolve({
+          postImages: [
+            {
+              id: 1,
+              title: 'first Post',
+              imageURL: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+              date: '2020-1-5',
+              content: 'post one content'
+            },
+            {
+              id: 2,
+              title: 'second Post',
+              imageURL: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+              date: '2020-1-15',
+              content: 'post two content'
+            },
+            {
+              id: 3,
+              title: 'thrid Post',
+              imageURL: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+              date: '2020-1-25',
+              content: 'post three content'
+            }
+          ]
+        })
+      }, 1500)
+    })
+      .then((data) => {
+        return data
+      })
+      .catch((e) => {
+        context.error(new Error())
+      })
   }
 }
 </script>
+
+
--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,3 +1,44 @@
 <template>
-  <h1>single post {{ $route.params .id}}</h1>
+  <v-container fluid>
+    <v-row class="text-center"></v-row>
+    <v-row>
+      <v-col cols="12" md="10" sm="12">
+        <h2 class="mb-3 purple--text text-uppercase" color="purple--text">{{ loadedPost.title }}</h2>
+        <!-- <v-img :src="blogPost.imageURL" elevation="4" class="ml-n6 mr-n12"></v-img> -->
+        <h4 class="font-weight-black mt-4">
+          {{loadedPost.date}}
+          <span class="primary--text">Admin</span>
+        </h4>
+
+        <p style=" font-size: 18px;" class="mt-4">{{ loadedPost.content }}</p>
+      </v-col>
+      <v-col cols="12" md="2" sm="12"></v-col>
+    </v-row>
+  </v-container>
 </template>
+<script>
+export default {
+  asyncData(context) {
+    return new Promise((resolve, rejected) => {
+      setTimeout(() => {
+        resolve({
+          loadedPost: {
+            id: 1,
+            title: 'Now a single Post (ID: " ' + context.params.id + ')',
+            imageURL: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+            date: '2020-1-5',
+            content:
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda incidunt sit adipisci aliquid facere dolorem quidem nihil deleniti corporis, ea iusto quasi corrupti eius debitis quisquam, hic accusamus totam pariatur.'
+          }
+        })
+      }, 1000)
+    })
+      .then((data) => {
+        return data
+      })
+      .catch((e) => {
+        context.error(new Error())
+      })
+  }
+}
+</script>
